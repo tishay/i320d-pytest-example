ffbeedffc4c7654b55ed9612d98700fdbff5e84c
Oluwatise Mobuse <ojm437@educcomp01.ccbb.utexas.edu>
2025-03-29 20:45:41 -0500

    Final submission commit

--- my_code.py
+++ my_code.py
@@ -1,15 +1,23 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
-    # given "5125558823". Split the parts, then recombine and return
+    # given "5125558823". Split the parts, then recombine and return\
+    
+    for char in phone_num_to_fix:  #For every character in the number, if it isn't a digit, the loop will correct the syntax
+        if not char.isdigit():
+            phone_num_to_fix = phone_num_to_fix.replace(char, "")
+            
+    if len(phone_num_to_fix) > 10: #If the length of the number is greater than 10, shorten it to first 10 digits
+        phone_num_to_fix = phone_num_to_fix[0:10]
+    
     if not phone_num_to_fix.isdigit():
         raise ValueError
+    
     area_code = phone_num_to_fix[0:3] # 512 (first three digits)
     three_part = phone_num_to_fix[3:6] # 555 (next three digits)
     four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
   
     fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
-    
     
   
     return fixed_num
