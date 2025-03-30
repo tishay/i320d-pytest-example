9ae4c87c6d18d8648c48e5aabd9486fec1ebe521
Oluwatise Mobuse <ojm437@educcomp01.ccbb.utexas.edu>
2025-03-29 19:58:16 -0500

    Tests should fail

--- my_code.py
+++ my_code.py
@@ -1,14 +1,24 @@
 import pytest
 
 def fix_phone_num(phone_num_to_fix):
-  # given "5125558823". Split the parts, then recombine and return
-  area_code = phone_num_to_fix[0:3] # 512 (first three digits)
-  three_part = phone_num_to_fix[3:6] # 555 (next three digits)
-  four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
+    # given "5125558823". Split the parts, then recombine and return
+    area_code = phone_num_to_fix[0:3] # 512 (first three digits)
+    three_part = phone_num_to_fix[3:6] # 555 (next three digits)
+    four_part = phone_num_to_fix[6:] # # 8823 (last four digits)
   
-  fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
+    fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part 
   
-  return fixed_num
+    return fixed_num
 
 def test_fix_phone_num():
-  assert fix_phone_num("5125558823") == '(512) 555 8823'
+    assert fix_phone_num("5125558823") == '(512) 555 8823'
+    assert fix_phone_num("5554429876") == '(555) 442 9876'
+    assert fix_phone_num("3216543333") == '(321) 654 3333'
+    
+def test_fix_phone_num_2():
+    assert fix_phone_num("555-442-9876") == '(555) 442 9876'
+    
+def test_fix_phone_num_3():
+    assert fix_phone_num("(321) 654 3333") == '(321) 654 3333'
+
+        
